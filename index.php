8d0a65af2b51e48e74fa4fb6c385adbe9a28c0b1
bsandesh <62261515+bsandesh@users.noreply.github.com>
2025-04-14 11:32:20 -0500

    Update index.php

--- index.php
+++ index.php
@@ -25,11 +25,15 @@
 		$result = $conn->query($sql);
 
 		if ($result->num_rows > 0) {
-			// Display table headers
-			echo "<table><tr><th>ID</th><th>Name</th><th>Email</th></tr>";
+			// Display table headers-modified sandesh 4/14/2025
+			echo "<table><tr><th>Employee Number</th><th>First Name</th><th>Email</th></tr>";
+			//echo "<table><tr><th>ID</th><th>Name</th><th>Email</th></tr>";
 			// Loop through results and display each row in the table
 			while($row = $result->fetch_assoc()) {
-				echo "<tr><td>" . $row["id"] . "</td><td>" . $row["name"] . "</td><td>" . $row["email"] . "</td></tr>";
+				// dispaly rows -modified sandesh 4/14/2025
+				 echo "<tr><td>" . $row["emp_no"] . "</td><td>" . $row["first_name"] . "</td><td>" . $row["email_id"] . "</td></tr>";
+
+				//echo "<tr><td>" . $row["id"] . "</td><td>" . $row["name"] . "</td><td>" . $row["email"] . "</td></tr>";
 			}
 			echo "</table>";
 		} else {
